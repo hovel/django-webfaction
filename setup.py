7472603a14b41bcf41ac9f50f7bc95cf29d731ed
zeus <gelios@gmail.com>
2011-08-12 12:56:02 +0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+
+from setuptools import setup
+
+
+setup(
+        name='django-webfaction',
+        version='1.0',
+        packages=['webfaction'],
+        author='Pavel Zhukov',
+        author_email='gelios@gmail.com',
+        description='Various tools to run django more seamless',
+        #long_description = open('README').read(),
+        license='GPL',
+        keywords='webfaction, django',
+        url='http://bitbucket.org/zeus/webfaction/'
+        )
