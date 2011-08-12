eb347721ba9158923de8cfdd83e3f11532b5d703
zeus <gelios@gmail.com>
2011-08-12 15:12:29 +0400

    Fix setup file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         author='Pavel Zhukov',
         author_email='gelios@gmail.com',
         description='Collection of tools to run django on webfaction more seamless',
-        long_description = open('README').read(),
+        long_description = open('README.rst').read(),
         license='GPL',
         keywords='webfaction, django',
         url='http://bitbucket.org/zeus/webfaction/'
